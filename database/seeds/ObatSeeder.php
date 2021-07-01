0ccc0fe8a1b095a2b10ad1b7f0f5e9ed648dcb81
Anisapandu23 <anisapandundis1@gmail.com>
2021-07-01 23:35:12 +0700

    seeder data obat

---
+++ database/seeds/ObatSeeder.php
@@ -0,0 +1,176 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Obat;
+
+class ObatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Obat::create([
+            'kode' => 'obt0001',
+            'nama' => 'BODREXIN FLU&BATUK BERDAHAK SYR 56ML',
+            'jenis' => 'Syrup',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '12.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0002',
+            'nama' => 'DECOLGEN TAB 10S',
+            'jenis' => 'Tablet',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '6.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0003',
+            'nama' => 'PANADOL COLD FLU REG TAB 10S HIJAU',
+            'jenis' => 'Tablet',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '15.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0004',
+            'nama' => 'OBH COMBI ANAK BATUK FLU RASA JERUK 60ML',
+            'jenis' => 'Syrup',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '16.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0005',
+            'nama' => 'DEXTRAL SYR 60ML',
+            'jenis' => 'Syrup',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '13.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0006',
+            'nama' => 'BENACOL EXPECT SYR 60ML',
+            'jenis' => 'Syrup',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '17.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0007',
+            'nama' => 'TUZALOS TAB 4 S',
+            'jenis' => 'Tablet',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '6.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0008',
+            'nama' => 'MOLEXFLU 150 TAB',
+            'jenis' => 'Tablet',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '6.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0009',
+            'nama' => 'LAPISIV-T TAB',
+            'jenis' => 'Tablet',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '18.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0010',
+            'nama' => 'RHINOS SR KAPSUL',
+            'jenis' => 'Kapsul',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '8.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0011',
+            'nama' => 'NASONEX 60DOSIS NASAL SPRAY 50MCG/DOSIS',
+            'jenis' => 'Spray',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '300.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0012',
+            'nama' => 'DIXOL ALKOHOL 70% 100ML',
+            'jenis' => 'Antiseptik Cair',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '11.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0013',
+            'nama' => 'PUREKIDS ITCHY CR 10G',
+            'jenis' => 'Salep',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '28.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0014',
+            'nama' => 'SENSI ALKOHOL SWAB 100S',
+            'jenis' => 'Tissue',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '24.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0015',
+            'nama' => 'DETTOL LIQ REG 500ML',
+            'jenis' => 'Antiseptik Cair',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '98.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0016',
+            'nama' => 'HELIATECH SUNCREEN SPF 45 LOT 30ML',
+            'jenis' => 'Salep',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '99.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0017',
+            'nama' => 'BETADINE SPRAY 55G',
+            'jenis' => 'Spray',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '93.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0018',
+            'nama' => 'MINYAK ANGIN V FRESH LAVENDER ROLL ON 8ML',
+            'jenis' => 'Roll On',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '12.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0019',
+            'nama' => 'CRESTOR 5MG TAB 30S',
+            'jenis' => 'Tablet',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '16.000',
+            'stok' => '5',
+        ]);
+        Obat::create([
+            'kode' => 'obt0020',
+            'nama' => 'PARACETAMOL MEF 500MG TAB 100S',
+            'jenis' => 'Tablet',
+            'kadaluarsa' => '2022-10-20',
+            'harga' => '5.000',
+            'stok' => '5',
+        ]);
+    }
+}
